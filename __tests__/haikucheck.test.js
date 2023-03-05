cda22c19d96fa8fa725e3ce05f23370fc8907133
Quin Asselin <quin.asselin1@gmail.com>
2023-03-05 13:59:48 -0500

    return good fail for first test of lineCount
    Co-authored-by: Athea DeLing <almdeling@gmail.com>
    Co-authored-by: Oscar Gonzalez <ogon318@gmail.com>

---
+++ __tests__/haikucheck.test.js
@@ -0,0 +1,10 @@
+import lineCount from '../src/haikucheck';
+
+describe('lineCount', () => {
+  test('receiving an input of one line should return a result of one', () => {
+    const newString = "This is a line.";
+    const numberOfLines = lineCount(newString);
+    expect(numberOfLines).toEqual(1);
+  });
+
+});
