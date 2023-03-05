04b1068655dcf0909b3665b8a8adb961bf13e73b
Quin Asselin <quin.asselin1@gmail.com>
2023-03-05 14:10:30 -0500

    verify output works for multiple lines
    Co-authored-by: Athea DeLing <almdeling@gmail.com>
    Co-authored-by: Oscar Gonzalez <ogon318@gmail.com>

--- __tests__/haikucheck.test.js
+++ __tests__/haikucheck.test.js
@@ -7,5 +7,10 @@ describe('lineCount', () => {
     expect(numberOfLines).toEqual(1);
   });
   
+  test('receiving an input of two or more lines should return a result of one', () => {
+    const newString = "This is a line. \n This is a second one. \n And a third";
+    const numberOfLines = lineCount(newString);
+    expect(numberOfLines).toEqual(3);
+  });
 
 });
