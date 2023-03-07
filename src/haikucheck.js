200f1739fde90381ef38c7e463c06c8c7d1f2389
Quin Asselin <quin.asselin1@gmail.com>
2023-03-07 00:01:53 -0500

    nested code within closure require one arg and fix conditional
    Co-authored-by: Oscar Gonzalez <ogon318@gmail.com>
    Co-authored-by: Greg Ramsower <greg.ramsower@gmail.com>

--- src/haikucheck.js
+++ src/haikucheck.js
@@ -4,7 +4,7 @@ export default function lineCount(passage) {
 }
 
 export function syllableCount(passage) {
-  const syllablePattern = /([bcdfghjklmnpqrstvwxyz]*)([aeiouy]+[bcdfghjklmnpqrstvwxyz]*)*/gi
+  const syllablePattern = /([bcdfghjklmnpqrstvwxyz]*)([aeiouy]+[bcdfghjklmnpqrstvwxyz]*)*/gi  
   const foundSyllables = passage.match(syllablePattern);
   console.log(foundSyllables);
   return foundSyllables.length;
@@ -18,3 +18,37 @@ export function syllableCount(passage) {
 // }
 
 //(?:[^laeiouy]es?|ed)$/
+
+
+export default function lineCount(passage) {
+  const passageArr = passage.split('\n')
+  return function syllableCount(passageArr) {
+    const syllableArr = passageArr.map(function (line) {
+      const lineArr = line.split(" ");
+      const lineSyllArr = lineArr.map(function (word) {
+        word = word.toLowerCase();
+        if(word.length <= 3) { 
+          return 1; 
+        }
+        word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');
+        word = word.replace(/^y/, '');
+        return word.match(/[aeiouy]{1,2}/g).length;
+      });
+      lineSyllArr.reduce((lineSyllCount, runningSum) => lineSyllCount + runningSum, 0);
+    });
+    if (syllableArr === [5,7,5]) { //make a real comparison!
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+}
+
+function new_count(word) {
+  word = word.toLowerCase();                                     //word.downcase!
+  if(word.length <= 3) { return 1; }                             //return 1 if word.length <= 3
+    word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');   //word.sub!(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '')
+    word = word.replace(/^y/, '');                                 //word.sub!(/^y/, '')
+    return word.match(/[aeiouy]{1,2}/g).length;                    //word.scan(/[aeiouy]{1,2}/).size
+}
