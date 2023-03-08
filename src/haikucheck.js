353a578559578c2122d844b2800de0ad5f586ae8
Quin Asselin <quin.asselin1@gmail.com>
2023-03-07 22:32:50 -0500

    lint issues with haikuCheck function and receive good test
    Co-authored-by: Athea DeLing <almdeling@gmail.com>
    Co-authored-by: Oscar Gonzalez <ogon318@gmail.com>
    Co-authored-by: Greg Ramsower <greg.ramsower@gmail.com>

--- src/haikucheck.js
+++ src/haikucheck.js
@@ -19,36 +19,68 @@ export function syllableCount(passage) {
 
 //(?:[^laeiouy]es?|ed)$/
 
+//lineCount("this is a real haiku")
+//lineCount("this is a real haiku")(conditioned input of the haiku in the previous param)
 
 export default function lineCount(passage) {
-  const passageArr = passage.split('\n')
-  return function syllableCount(passageArr) {
-    const syllableArr = passageArr.map(function (line) {
-      const lineArr = line.split(" ");
-      const lineSyllArr = lineArr.map(function (word) {
-        word = word.toLowerCase();
-        if(word.length <= 3) { 
-          return 1; 
-        }
-        word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');
-        word = word.replace(/^y/, '');
-        return word.match(/[aeiouy]{1,2}/g).length;
-      });
-      lineSyllArr.reduce((lineSyllCount, runningSum) => lineSyllCount + runningSum, 0);
+  const passageArr = passage.split('\n');
+  console.log("passageArr: " + passageArr);
+  const syllableArr = passageArr.map(function (line, index) {
+    const lineArr = line.split(" ");
+    console.log("syllableArr index[" + index + "]:" + lineArr);
+    const lineSyllArr = lineArr.map(function (word) {
+      word = word.toLowerCase();
+      if(word.length < 1) {
+        return 0;
+      }
+      if(word.length <= 3) { 
+        return 1; 
+      }
+      word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');
+      word = word.replace(/^y/, '');
+      return word.match(/[aeiouy]{1,2}/g).length;
     });
-    if (syllableArr === [5,7,5]) {//syllableArr[0] === 5 && syllableArr[1] === 7 && syllableArr[2] === 5 && syllableArr.length === 3)
-      return true;
-    }
-    else {
-      return false;
-    }
+    console.log("lineSyllArr: " + lineSyllArr);
+    return lineSyllArr.reduce((lineSyllCount, runningSum) => lineSyllCount + runningSum);
+  });
+  console.log("syllableArr: " + syllableArr);
+  if (syllableArr[0] === 5 && syllableArr[1] === 7 && syllableArr[2] === 5 && syllableArr.length === 3) {
+    return true;
+  }
+  else {
+    return false;
   }
 }
 
-function new_count(word) {
-  word = word.toLowerCase();                                     //word.downcase!
-  if(word.length <= 3) { return 1; }                             //return 1 if word.length <= 3
-    word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');   //word.sub!(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '')
-    word = word.replace(/^y/, '');                                 //word.sub!(/^y/, '')
-    return word.match(/[aeiouy]{1,2}/g).length;                    //word.scan(/[aeiouy]{1,2}/).size
-}
+// export default function lineCount(passage) {
+//   const passageArr = passage.split('\n')
+//   return function syllableCount(passageArr) {
+//     const syllableArr = passageArr.map(function (line) {
+//       const lineArr = line.split(" ");
+//       const lineSyllArr = lineArr.map(function (word) {
+//         word = word.toLowerCase();
+//         if(word.length <= 3) { 
+//           return 1; 
+//         }
+//         word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');
+//         word = word.replace(/^y/, '');
+//         return word.match(/[aeiouy]{1,2}/g).length;
+//       });
+//       lineSyllArr.reduce((lineSyllCount, runningSum) => lineSyllCount + runningSum, 0);
+//     });
+//     if (syllableArr === [5,7,5]) {//syllableArr[0] === 5 && syllableArr[1] === 7 && syllableArr[2] === 5 && syllableArr.length === 3) \n
+//       return true;
+//     }
+//     else {
+//       return false;
+//     }
+//   }
+// }
+
+// function new_count(word) {
+//   word = word.toLowerCase();                                     //word.downcase!
+//   if(word.length <= 3) { return 1; }                             //return 1 if word.length <= 3
+//     word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');   //word.sub!(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '')
+//     word = word.replace(/^y/, '');                                 //word.sub!(/^y/, '')
+//     return word.match(/[aeiouy]{1,2}/g).length;                    //word.scan(/[aeiouy]{1,2}/).size
+// }
